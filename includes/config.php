51b790f97223cf418efec8146931f0036ecceaad
Vova Feldman <vova@freemius.com>
2016-07-07 00:12:22 -0700

    Cleaned up config.

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,4 @@
 <?php
-    define('PAGE_2_IMAGES_REST_KEY', '8b47255a0b3fe11b');
-    define('PAGE_2_IMAGES_KEY', '7f590b52d5afdfd7');
-    define('GITHUB_ACCESS_TOKEN', '14bca731652ded61f28d130f5fa1657be7163fa9');
+    define('PAGE_2_IMAGES_REST_KEY', 'YOUR_P2I_REST_KEY');
+    define('PAGE_2_IMAGES_KEY',      'YOUR_P2I_KEY');
+    define('GITHUB_ACCESS_TOKEN',    'YOUR_GITHUB_ACCESS_TOKEN');
