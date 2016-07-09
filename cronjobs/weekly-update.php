6c446708e9069f2be3ea98bdb66e8dd010cd4841
Vova Feldman <vova@freemius.com>
2016-07-08 17:25:47 -0700

    [weekly-update] [cron] Made the weekly frameworks update more intelligent. Don't update a framework unless its data was last fetched more than a week ago (or never before).

--- cronjobs/weekly-update.php
+++ cronjobs/weekly-update.php
@@ -2,21 +2,38 @@
     require_once dirname(__DIR__) . '/includes/config.php';
     require_once dirname(__DIR__) . '/includes/functions.php';
 
+    /* Frameworks update
+ -------------------------------------------------------------------------------------*/
     $framework_files        = glob(dirname(__DIR__) . '/src/frameworks/*.php');
     $framework_files_output = dirname(__DIR__) . '/src/frameworks/compiled/';
+    $index_path             = $framework_files_output . '_index.php';
+    $updates_index_path     = $framework_files_output . '_updates.php';
 
     $frameworks_index = array();
+    $frameworks_updates = array();
+
+    if (file_exists($updates_index_path))
+        require_once $updates_index_path;
 
     foreach ($framework_files as $key => $file)
     {
-        $file = str_replace('\\', '/', $file);
-        $file = preg_replace('|/+|', '/', $file);
+        $file = canonize_file_path($file);
 
+        $slug = substr($file, strrpos($file, '/') + 1, - 4);
+        $framework_path = $framework_files_output . substr($file, strrpos($file, '/') + 1);
+
+        if (isset($frameworks_updates[$slug]) && $frameworks_updates[$slug] >= (time() - WEEK_IN_SEC) && file_exists($framework_path))
+        {
+            // Get framework cached data.
+            require_once $framework_path;
+        }
+        else
+        {
         // Get framework data.
         require_once $file;
 
         // Get slug from filename.
-        $framework['slug'] = substr($file, strrpos($file, '/') + 1, - 4);
+            $framework['slug'] = $slug;
 
         // Enrich with GitHub info.
         if (isset($framework['github_repo']))
@@ -29,19 +46,29 @@
         if ( ! isset($framework['banner']))
             enrich_banner($framework);
 
+            // Add banner protocol if missing before running via CDN.
+            if ('/' === $framework['banner'][0])
+                $framework['banner'] = 'http:' . $framework['banner'];
+
         // Add images CDN proxy.
         $framework['banner'] = 'https://res.cloudinary.com/freemius/image/fetch/' . $framework['banner'];
 
-        $framework_path = $framework_files_output . substr($file, strrpos($file, '/') + 1);
+            dump_var_to_php_file($framework, '$framework', $framework_path);
 
-        dump_framework_to_php_file($framework, $framework_files_output . $framework_path);
+            $frameworks_updates[$slug] = time();
+        }
 
-        $frameworks_index[$framework['slug']] = $framework['github']['stars'];
+        $frameworks_index[$slug] = $framework['github']['stars'];
     }
 
+    // Sort frameworks by stars.
     arsort($frameworks_index);
 
-    $index_path = $framework_files_output . 'index.php';
+    // Dump index.
+    dump_var_to_php_file($frameworks_index, '$frameworks_index', $index_path);
+
+    // Dump framework update timestamp.
+    dump_var_to_php_file($frameworks_updates, '$frameworks_updates', $updates_index_path);
 
     if (file_exists($index_path))
         unlink($index_path);
--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,4 @@
 <?php
     define('PAGE_2_IMAGES_REST_KEY', 'YOUR_P2I_REST_KEY');
     define('PAGE_2_IMAGES_KEY',      'YOUR_P2I_KEY');
-    define('GITHUB_ACCESS_TOKEN',    'YOUR_GITHUB_ACCESS_TOKEN');
+    define('GITHUB_ACCESS_TOKEN',    'YOUR_GITHUB_ACCESS_TOKEN');    define('WEEK_IN_SEC', 60 * 60 * 24 * 7);
