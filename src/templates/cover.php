45453326a588982980340866f16fa52272d3ed64
Vova Feldman <vova@freemius.com>
2016-07-08 17:21:03 -0700

    [github] Added a button to star the repo on github.

--- src/templates/cover.php
+++ src/templates/cover.php
@@ -8,6 +8,8 @@
         <p class="powered-by">Powered with <b>❤</b> by <b><a
                     href="https://freemius.com?utm_source=includewp_home&utm_medium=link&utm_content=header&utm_campaign=includewp">Freemius</a></b>
         </p>
+        <!-- Place this tag where you want the button to render. -->
+        <a class="github-button" href="https://github.com/Freemius/IncludeWP" data-style="mega" data-count-href="/Freemius/IncludeWP/stargazers" data-count-api="/repos/Freemius/IncludeWP#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star ntkme/github-buttons on GitHub">Star</a>
     </div>
 </div>
 <!--/ Cover -->
--- src/templates/footer.php
+++ src/templates/footer.php
@@ -47,4 +47,4 @@
 <!--/ Footer -->
 
 <!-- Place this tag in your head or just before your close body tag. -->
-<!--<script async defer src="https://buttons.github.io/buttons.js"></script>-->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
