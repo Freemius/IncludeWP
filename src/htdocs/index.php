cbc905b9612d03a49a8ffeff5605bb40873d3261
Vova Feldman <vova@freemius.com>
2016-07-20 01:28:38 -0400

    [open-graph] [sharing] [framework] Adjusted the sharing params to fetch the framework's name, desc & URL when on a specific framework.

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,5 +1,31 @@
-<?php define('NAME', 'IncludeWP') ?>
-<?php define('SITE_ADDRESS', 'https://includewp.com') ?>
+<?php
+    define('NAME', 'IncludeWP');
+    define('SITE_ADDRESS', 'https://includewp.com');
+
+    $framework_slug = ! empty($_REQUEST['framework']) ? $_REQUEST['framework'] : '';
+    $framework_slug = preg_replace("/[^A-Za-z0-9\\_\\-]/", '', $framework_slug);
+
+    if ( ! file_exists(dirname(__DIR__) . '/frameworks/compiled/' . $framework_slug . '.php'))
+        $framework_slug = '';
+
+    $title          = 'Top Frameworks for WordPress Plugin & Theme Developers';
+    $og_title       = 'IncludeWP';
+    $og_url         = SITE_ADDRESS;
+    $og_description = 'IncludeWP is a leaderboard of the top open-source frameworks for WordPress plugin & theme developers. Because code reusability is awesome.';
+    if ( ! empty($framework_slug))
+    {
+        include dirname(__DIR__) . '/frameworks/compiled/' . $framework_slug . '.php';
+
+        $framework_name = isset($framework['full_name']) ?
+            $framework['full_name'] :
+            $framework['name'];
+
+        $title          = htmlentities($framework_name);
+        $og_title       = $title . ' &ndash; IncludeWP';
+        $og_url         = $og_url . '/' . $framework_slug . '/';
+        $og_description = $framework['description'];
+    }
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
@@ -9,37 +35,35 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 
-    <link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
+    <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
 
     <!-- Always force latest IE rendering engine or request Chrome Frame -->
     <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
 
-    <title>Top Frameworks for WordPress Plugin & Theme Developers &ndash; IncludeWP</title>
+    <title><?php echo $title ?> &ndash; IncludeWP</title>
 
-    <meta name="description"
-          content="IncludeWP is a leaderboard of the top open-source frameworks for WordPress plugin & theme developers. Because code reusability is awesome."/>
+    <meta name="description" content="<?php echo $og_description ?>"/>
 
     <!-- Open Graph data -->
-    <meta property="og:title" content="IncludeWP"/>
-    <meta property="og:type" content="website" />
+    <meta property="og:title" content="<?php echo $og_title ?>"/>
+    <meta property="og:type" content="website"/>
     <meta property="og:image" content="<?php echo SITE_ADDRESS ?>/assets/img/og-includewp.png"/>
-    <meta property="og:url" content="<?php echo SITE_ADDRESS ?>"/>
-    <meta property="og:description"
-          content="IncludeWP is a leaderboard of the top open-source frameworks for WordPress plugin & theme developers. Because code reusability is awesome."/>
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:site_name" content="IncludeWP" />
-    <meta property="fb:admins" content="598827889" />
-    <meta property="fb:app_id" content="132877816722342" />
+    <meta property="og:url" content="<?php echo $og_url ?>"/>
+    <meta property="og:description" content="<?php echo $og_description ?>"/>
+    <meta property="og:locale" content="en_US"/>
+    <meta property="og:site_name" content="IncludeWP"/>
+    <meta property="fb:admins" content="598827889"/>
+    <meta property="fb:app_id" content="132877816722342"/>
 
     <!-- Twitter Card data -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@freemius">
     <meta name="twitter:creator" content="@vovafeldman">
-    <meta name="twitter:title" content="IncludeWP">
-    <meta name="twitter:description" content="IncludeWP is a leaderboard of the top open-source frameworks for WordPress plugin & theme developers. Because code reusability is awesome.">
+    <meta name="twitter:title" content="<?php echo $og_title ?>">
+    <meta name="twitter:description" content="<?php echo $og_description ?>">
     <meta name="twitter:image:src" content="<?php echo SITE_ADDRESS ?>/assets/img/og-includewp.png">
 
-<!--    <link rel="canonical" href="--><?php //echo $canonical; ?><!--" />-->
+    <!--    <link rel="canonical" href="--><?php //echo $canonical; ?><!--" />-->
 </head>
 <body>
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
@@ -66,7 +90,7 @@
 
 <?php require dirname(__DIR__) . '/templates/filters.php' ?>
 
-<?php require dirname(__DIR__) . '/templates/frameworks.php' ?>
+    <?php require dirname(__DIR__) . '/templates/frameworks.php' ?>
 
 <?php require dirname(__DIR__) . '/templates/footer.php' ?>
 
