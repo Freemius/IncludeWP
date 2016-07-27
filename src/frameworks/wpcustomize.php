94476acf69f0d4415070173c104da43fa7947cbf
Vova Feldman <vova.feldman@gmail.com>
2016-07-27 10:32:43 -0400

    [description] [minor]

--- src/frameworks/wpcustomize.php
+++ src/frameworks/wpcustomize.php
@@ -2,7 +2,7 @@
     $framework = array(
         'name'           => 'WPCustomize',
         'full_name'      => 'WPCustomize',
-        'description'    => 'WP Customize boilerplate theme, and features implementation.',
+        'description'    => 'Customizer boilerplate for plugins and themes (currently a theme based features implementation).',
         'github_repo'    => 'ahmadawais/WPCustomize',
         'homepage'       => 'https://labs.ahmadawais.com/wpcustomize/',
         'is_for_plugins' => true,
