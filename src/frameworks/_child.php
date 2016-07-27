6c3620749cc0b34e288a719884bdab6a2395df82
Vova Feldman <vova.feldman@gmail.com>
2016-07-27 10:20:51 -0400

    [description] [minor]

--- src/frameworks/_child.php
+++ src/frameworks/_child.php
@@ -2,7 +2,7 @@
     $framework = array(
         'name'           => '_child',
         'full_name'      => '_child',
-        'description'    => '_child is a WordPress Child Theme Boilerplate.',
+        'description'    => '_child is a child theme boilerplate.',
         'github_repo'    => 'ahmadawais/_child',
         'homepage'       => 'https://labs.ahmadawais.com/_child/',
         'is_for_plugins' => false,
