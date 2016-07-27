32a71b8dd92b4e0d5574ac1cf7ff9224a191c6c7
Chris Blakley <greatblakes@gmail.com>
2016-07-27 13:01:19 -0400

    Add Nebula theme framework
    Adds the Nebula WordPress theme

---
+++ src/frameworks/nebula.php
@@ -0,0 +1,14 @@
+<?php
+    $framework = array(
+        'name'           => 'Nebula',
+        'description'    => 'Advanced Starter WordPress Theme for Developers.',
+        'github_repo'    => 'chrisblakley/Nebula',
+        'wp_slug'        => 'Nebula',
+        'homepage'       => 'https://gearside.com/nebula',
+        'is_for_plugins' => false,
+        'is_for_themes'  => true,
+        'thumbprint'     => array(
+            'file'  => 'nebula.php',
+            'token' => 'class Nebula',
+        ),
+    );
