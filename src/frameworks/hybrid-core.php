9f67b7b601f7d29672285643d50860bfbb50795f
Justin Tadlock <justin@justintadlock.com>
2016-07-27 11:15:42 -0500

    Adds the Hybrid Core theme framework.

---
+++ src/frameworks/hybrid-core.php
@@ -0,0 +1,14 @@
+<?php
+    $framework = array(
+        'name'           => 'Hybrid Core',
+        'full_name'      => 'Hybrid Core Theme Framework',
+        'description'    => 'Give your WordPress theme superpowers.',
+        'github_repo'    => 'justintadlock/hybrid-core',
+        'homepage'       => 'http://themehybrid.com/hybrid-core',
+        'is_for_plugins' => false,
+        'is_for_themes'  => true,
+        'thumbprint'     => array(
+            'file'  => 'hybrid.php',
+            'token' => 'class Hybrid',
+        ),
+    );
