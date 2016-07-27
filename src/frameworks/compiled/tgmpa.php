a39989fc184f6a3eecc14da879c2f6c75877fccb
Gary Jones <gamajo@gamajo.com>
2016-07-27 14:43:46 +0100

    Update TGM to TGMPA (compiled frameworks)

--- src/frameworks/compiled/tgmpa.php
+++ src/frameworks/compiled/tgmpa.php
@@ -1,5 +1,5 @@
 <?php $framework = array (
-  'name' => 'TGM',
+  'name' => 'TGMPA',
   'full_name' => 'TGM Plugin Activation',
   'description' => 'Easy way to require and recommend plugins.',
   'github_repo' => 'TGMPA/TGM-Plugin-Activation',
@@ -11,12 +11,12 @@
     'file' => 'class-tgm-plugin-activation.php',
     'token' => 'class TGM_Plugin_Activation',
   ),
-  'slug' => 'tgm',
+  'slug' => 'tgmpa',
   'github' => 
   array (
     'stars' => 1169,
-    'forks' => 343,
-    'issues' => 58,
+    'forks' => 342,
+    'issues' => 59,
   ),
   'banner' => 'https://res.cloudinary.com/freemius/image/fetch/http://api.page2images.com/ccimages/11/33/i28cHAaNRkqaI29r.png',
 );
