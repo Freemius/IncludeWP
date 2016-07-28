885355a28f1ab787c4c8fb2993467a5dd6bf54f0
Justin Tadlock <justin@justintadlock.com>
2016-07-28 09:43:45 -0500

    Update Hybrid Core description.

--- src/frameworks/hybrid-core.php
+++ src/frameworks/hybrid-core.php
@@ -2,7 +2,7 @@
     $framework = array(
         'name'           => 'Hybrid Core',
         'full_name'      => 'Hybrid Core Theme Framework',
-        'description'    => 'Give your WordPress theme superpowers.',
+        'description'    => 'Theme development toolbox.',
         'github_repo'    => 'justintadlock/hybrid-core',
         'homepage'       => 'http://themehybrid.com/hybrid-core',
         'is_for_plugins' => false,
