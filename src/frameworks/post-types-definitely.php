4219a36211b96a4a3b98dccc0e110617b1284ebb
Felix Arntz <felix-arntz@t-online.de>
2016-07-28 12:39:18 +0200

    Adds the Post Types Definitely framework

---
+++ src/frameworks/post-types-definitely.php
@@ -0,0 +1,14 @@
+<?php
+    $framework = array(
+        'name'           => 'Post Types Definitely',
+        'description'    => 'Comprehensive framework for post types and taxonomies with meta UI.',
+        'github_repo'    => 'felix-arntz/post-types-definitely',
+        'wp_slug'        => 'post-types-definitely',
+        'homepage'       => 'https://github.com/felixarntz/post-types-definitely/wiki',
+        'is_for_plugins' => true,
+        'is_for_themes'  => false,
+        'thumbprint'     => array(
+            'file'  => 'post-types-definitely.php',
+            'token' => "'Post Types Definitely'",
+        ),
+    );
