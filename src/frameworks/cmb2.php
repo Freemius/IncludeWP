0d03c89fd4e8b2fa9e0f847c71762e31f0c44e49
Justin Sternberg <me@jtsternberg.com>
2016-07-28 10:53:18 -0400

    Update CMB2 info.
    The full name for CMB2 is CMB2. :)

--- src/frameworks/cmb2.php
+++ src/frameworks/cmb2.php
@@ -1,8 +1,8 @@
 <?php
     $framework = array(
         'name'           => 'CMB2',
-        'full_name'      => 'Custom Metaboxes 2',
-        'description'    => 'Toolkit for building metaboxes, custom fields, and forms.',
+        'full_name'      => 'CMB2',
+        'description'    => "CMB2 is a developer's toolkit for building metaboxes, custom fields, and forms for WordPress that will blow your mind.",
         'github_repo'    => 'WebDevStudios/CMB2',
         'wp_slug'        => 'cmb2',
         'homepage'       => 'http://cmb2.io/',
