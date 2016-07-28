d6c4e091e1542552b0773fec0c11c277262a640f
Thach Ngoc Pham <ezit92@gmail.com>
2016-07-28 17:25:01 +0200

    Add Beans Framework <beans.php>

---
+++ src/frameworks/beans.php
@@ -0,0 +1,9 @@
+<?php
+    $framework = array(
+        'name'           => 'Beans',
+        'description'    => 'Beans is an incredibly powerful and flexible WordPress theme, yet light weight and unbelievably fast. The magic is under the hood!.',
+        'github_repo'    => 'Getbeans/Beans',
+        'homepage'       => 'http://www.getbeans.io/',
+        'is_for_plugins' => false,
+        'is_for_themes'  => true,
+    );
