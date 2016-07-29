262b605ff52d8e5cb94ee23bc01d609d28337e26
moldcraft <moldcraft@email.com>
2016-07-29 14:25:31 +0300

    added Unyson Framework

---
+++ src/frameworks/compiled/unyson.php
@@ -0,0 +1,32 @@
+<?php $framework = array (
+  'name'           => 'Unyson',
+  'full_name'      => 'Unyson Framework',
+  'description'    => 'Drag & Drop Page Builder, Options, Extensions and More',
+  'github_repo'    => 'ThemeFuse/Unyson',
+  'wp_slug'        => 'unyson',
+  'homepage'       => 'http://unyson.io/',
+  'is_for_plugins' => true,
+  'is_for_themes'  => true,
+  'thumbprint'     => array(
+    'file'  => 'framework/bootstrap.php',
+    'token' => "'FW'",
+  ),
+  'slug' => 'unyson',
+  'github' => 
+  array (
+    'stars' => 561,
+    'forks' => 124,
+    'issues' => 200,
+  ),
+  'wordpress' => 
+  array (
+    'name' => 'Unyson',
+    'short_description' => 'A simple and easy way to build a powerful website.',
+    'homepage' => 'http://wordpress.org/plugins/unyson',
+    'downloads' => 193756,
+    'active' => 30000,
+    'avg_rate' => '4.7',
+    'votes' => 30,
+  ),
+  'banner' => 'https://res.cloudinary.com/freemius/image/fetch/http://ps.w.org/unyson/assets/banner-772x250.png?rev=1034271',
+);
---
+++ src/frameworks/unyson.php
@@ -0,0 +1,15 @@
+<?php
+$framework = array(
+	'name'           => 'Unyson',
+	'full_name'      => 'Unyson Framework',
+	'description'    => 'Drag & Drop Page Builder, Options, Extensions and More',
+	'github_repo'    => 'ThemeFuse/Unyson',
+	'wp_slug'        => 'unyson',
+	'homepage'       => 'http://unyson.io/',
+	'is_for_plugins' => true,
+	'is_for_themes'  => true,
+	'thumbprint'     => array(
+		'file'  => 'framework/bootstrap.php',
+		'token' => "'FW'",
+	),
+);
