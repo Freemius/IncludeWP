aa21c6ce7bcc2627c7143a3f75e5a98e84132d63
Vova Feldman <vova@freemius.com>
2016-08-03 11:30:15 -0400

    [cover] Added ProductHunt upvote button to header.

--- src/templates/cover.php
+++ src/templates/cover.php
@@ -8,6 +8,7 @@
         <p class="powered-by">Powered with <b>❤</b> by <b><a
                     href="https://freemius.com?utm_source=includewp_home&utm_medium=link&utm_content=header&utm_campaign=includewp">Freemius</a></b>
         </p>
+        <iframe src="https://yvoschaap.com/producthunt/counter.html#href=http%3A%2F%2Fwww.producthunt.com%2Fr%2F7a6c6e0abf4c21%2F71022&amp;layout=wide" width="120" height="25" scrolling="no" frameborder="0" allowtransparency="true"></iframe>
         <!-- Place this tag where you want the button to render. -->
         <a class="github-button" href="https://github.com/Freemius/IncludeWP" data-style="mega" data-count-href="/Freemius/IncludeWP/stargazers" data-count-api="/repos/Freemius/IncludeWP#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star ntkme/github-buttons on GitHub">Star</a>
     </div>
