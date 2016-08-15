a896978a8d13218bc2126dfce144c62ab48f1b0a
Vova Feldman <vova.feldman@gmail.com>
2016-08-14 20:16:04 -0400

    [cherry] [description]

--- src/frameworks/cherry.php
+++ src/frameworks/cherry.php
@@ -2,9 +2,8 @@
     $framework = array(
         'name'           => 'Cherry',
         'full_name'      => 'Cherry Framework',
-        'description'    => 'A straightforward experience in web design. Easy install, shortcodes and more!',
+        'description'    => 'Easy to use WYSISYG theme framework.',
         'github_repo'    => 'CherryFramework/CherryFramework',
-        'wp_slug'        => '',
         'homepage'       => 'http://www.cherryframework.com/',
         'is_for_plugins' => false,
         'is_for_themes'  => true,
