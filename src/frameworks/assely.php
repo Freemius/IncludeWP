a69d8efd9fd79888a12ffeaf882e95f9c3512ddf
Jędrzej Chałubek <jedrzej.chalubek@gmail.com>
2016-12-15 17:18:08 +0100

    Create assely.php

---
+++ src/frameworks/assely.php
@@ -0,0 +1,10 @@
+<?php
+    $framework = array(
+        'name'           => 'Assely',
+        'full_name'      => 'Assely Framework',
+        'description'    => 'Modern PHP framework which brings a little joy to the WordPress development.',
+        'github_repo'    => 'assely/framework',
+        'homepage'       => 'https://assely.org',
+        'is_for_plugins' => true,
+        'is_for_themes'  => true,
+    );
