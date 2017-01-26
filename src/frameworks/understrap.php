a086684a7671cdd4f2ee4d370019f953227f68cb
koenemann <holger.koenemann@smartsteuer.de>
2017-01-26 12:07:18 +0100

    adding understrap.com

---
+++ src/frameworks/understrap.php
@@ -0,0 +1,11 @@
+<?php
+    $framework = array(
+        'name'           => 'UnderStrap',
+        'description'    => 'UnderStrap combines the Underscores starter theme (by Automattic) and the mobil-first, responsive grid framework
+Bootstrap 4 (by Twitter) into a perfect open source foundation for your next WordPress theme project.',
+        'github_repo'    => 'holger1411/understrap',
+        'homepage'       => 'https://understrap.com/',
+        'is_for_plugins' => false,
+        'is_for_themes'  => true,
+        ),
+    );
