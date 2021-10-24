f37cc811adb02edefc03e694db11eece0312d0c5
AL EMRAN <emrancu1@gmail.com>
2021-10-24 09:42:11 +0600

    Added "PluginMaster, a WordPress plugin development framework."
    PluginMaster is a WordPress plugin development framework with good structure, easy & elegant syntax. Build your application for WordPress with PluginMaster for better structure, quick development.
    Visit https://www.wppluginmaster.com for an overview

---
+++ src/frameworks/plugin-master.php
@@ -0,0 +1,11 @@
+<?php
+    $framework = array(
+        'name'           => 'PluginMaster',
+        'full_name'      => 'WordPress plugin development framework',
+        'description'    => 'PluginMaster is a WordPress plugin development framework with good structure, easy & elegant syntax. Build your application for WordPress with PluginMaster for better structure, quick development. Just visit for details: https://wppluginmaster.com',
+        'github_repo'    => 'WP-Plugin-Master/PluginMaster',
+        'homepage'       => 'https://www.wppluginmaster.com',
+        'is_for_plugins' => true,
+        'is_for_themes'  => false,
+        'thumbprint'     => array(),
+    );
